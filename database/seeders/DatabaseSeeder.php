796193b1c9ae76d6b563876d0c78a419511a6827
Archie Manayon <archmanayon@gmail.com>
2024-02-12 22:15:27 +0800

    can seed, but needs fixing with length of every entry

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Pulishing;
+use App\Models\User;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -11,12 +13,13 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
+    {                
         // \App\Models\User::factory(10)->create();
+        \App\Models\Pulishing::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        //     'email' => 'test@example.com', 
         // ]);
     }
 }
--- database/seeders/PulishingSeeder.php
+++ database/seeders/PulishingSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PulishingSeeder extends Seeder
@@ -12,6 +12,6 @@ class PulishingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //  \App\Models\Pulishing::factory(10)->create();
     }
 }
